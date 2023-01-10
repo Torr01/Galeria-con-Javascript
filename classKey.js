91ad83a9184f0cdcecaa1b1384de82047bc193bd
Torr01 <otormet@gmail.com>
2023-01-09 23:12:24 -0400

    addicion de input form

--- classKey.js
+++ classKey.js
@@ -1,22 +1,50 @@
-const contenedor = document.querySelector(".flex-container");
+const contenedor = document.querySelectorAll(".flex-container");
 
+// const text_title = document.getElementById("title");
+// const text_model = document.getElementById("model");
+// const text_price = document.getElementById("price");
 
-const crearLlave = (name, model, price) => {
+// const enter_button = document.getElementById("create");
+
+// let text1 = text_title.value;
+// let text2 = text_model.value;
+// let text3 = text_price.value;
+
+const crearLlave = (title, model, price) => {
     img = `<img class="llave-img", src="davy_jones_key.jpg">`;
-    name = `<h2>${name}</h2>`;
+    title = `<h2>${title}</h2>`;
     model = `<h3>${model}</h3>`;
     price = `<p>Precio: <b>$${price}</b></p>`;
-    return [img, name, model, price]
+    return [img, title, model, price]
 }
 
-const crearElementoLlave = (name, model, price) => {
-    let key = crearLlave(name, model, price);
-    let div = document.createElement("DIV");
+const crearElementoLlave = (title, model, price) => {
+    let key = crearLlave(title, model, price);
+    let div = document.createElement('DIV');
+    div.tabIndex = 1;
     div.classList.add(`item-1`,'flex-item');
     div.innerHTML = key[0] + key[1] + key[2] + key[3];
     contenedor.appendChild(div);
 }
 
-crearElementoLlave("llave", "xxxx", "32")
-crearElementoLlave("llave", "xxxx", "32")
-crearElementoLlave("llave", "xxxx", "32")
+
+let items = [];
+
+const addItem = (ev) => {
+    ev.preventDefault();
+    let item = {
+        title: document.getElementById('title').value,
+        model: document.getElementById('model').value,
+        price: document.getElementById('price').value
+    }
+
+    items.push(item);
+    console.log(items);
+    document.querySelector('.input-form').reset();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('create').addEventListener('click', addItem);
+    console.log(items);
+
+});
